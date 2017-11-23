53ff41ac0e9f03b68fecbf4803808b7be27e58fa
Julien Osouf <osoufjulie@eisti.eu>
2017-11-23 13:02:06 +0100

    - generate capteurs classes

---
+++ gen-capteurs/src/magic/TemperatureCapteur.java
@@ -0,0 +1,13 @@
+package magic;
+
+public class TemperatureCapteur extends Capteur{
+    @Override
+    protected void prepare(String dest) {
+
+    }
+
+    @Override
+    protected void generate() {
+
+    }
+}
