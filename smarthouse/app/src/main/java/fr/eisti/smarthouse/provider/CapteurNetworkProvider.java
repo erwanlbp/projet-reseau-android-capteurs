5e6d9afbea51fe1fc2b9f5014a8ca93a44ac08b0
erwanlbp <erwan.lbp@gmail.com>
2017-11-30 20:38:19 +0100

    Envoi un UDP à gencapteur pour switcher un capteur

---
+++ smarthouse/app/src/main/java/fr/eisti/smarthouse/provider/CapteurNetworkProvider.java
@@ -0,0 +1,47 @@
+package fr.eisti.smarthouse.provider;
+
+import android.app.Activity;
+import android.support.design.widget.Snackbar;
+import android.widget.Toast;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+/**
+ * Created by ErwanLBP on 30/11/17.
+ */
+
+public class CapteurNetworkProvider {
+
+    private static final String TAG = "FirebaseCapteurProvider";
+
+
+    public static void switchActiv(Activity activity, String capteurName, boolean activ) {
+        if (capteurName == null) {
+            Toast.makeText(activity, "Can't activate capteur because name is null", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO Recupérer le port d'écoute et l'adresse du serveur pour l'appli #10
+        int serverPort = 7777;
+        String msg = formSwitchActivMessage(capteurName, activ);
+        Toast.makeText(activity,msg,Toast.LENGTH_SHORT).show();
+
+        try {
+            DatagramSocket ds = new DatagramSocket();
+            InetAddress server = InetAddress.getByName("192.168.1.35");
+            int msgLength = msg.length();
+            byte[] msgByte = msg.getBytes();
+            DatagramPacket dp = new DatagramPacket(msgByte, msgLength, server, serverPort);
+            ds.send(dp);
+        } catch (Exception e) {
+            Toast.makeText(activity, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private static String formSwitchActivMessage(String capteurName, boolean activ) {
+        return capteurName + " : " + activ;
+    }
+
+}
