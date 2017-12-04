11353fdb76c4d78cbcfc7500750492baff35b91f
Julien Osouf <osoufjulie@eisti.eu>
2017-12-04 15:54:21 +0100

    - added help to main

--- gencapteurs/src/main/java/Main.java
+++ gencapteurs/src/main/java/Main.java
@@ -7,6 +7,17 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        //Si on a pas d'argument ou si le premier est "help" afficher help
+        if (args.length == 0 || args[0].equals("help")) {
+            System.out.println("#### HELP ####");
+            System.out.println("Commande à lancer : java -jar <jarname> <ip> <mode>");
+            System.out.println("Modes :");
+            System.out.println("- send/listen : Générer les flux et écouter les commandes de start et stop");
+            System.out.println("- listen : Ecouter les commandes de start et stop");
+            System.out.println("- stop : Envoyer commande de stop d'un LightCapteur");
+            return;
+        }
+
         String ip = args[0];
         int portEnvoi = Integer.parseInt(args[1]);
         int portEcoute = Integer.parseInt(args[2]);
