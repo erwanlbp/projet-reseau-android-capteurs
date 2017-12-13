8bef1d8d336b49156fabf56e268fd6a765a06824
Julien Osouf <osoufjulie@eisti.eu>
2017-12-13 16:30:13 +0100

    activer/désactiver capteur vocalement en mieux

--- smarthouse/app/src/main/java/fr/eisti/smarthouse/presenter/CapteursListPresenter.java
+++ smarthouse/app/src/main/java/fr/eisti/smarthouse/presenter/CapteursListPresenter.java
@@ -38,36 +38,67 @@ public class CapteursListPresenter {
     }
 
     public void vocalActivateCapteur(String sentence, CapteursListAdapter adapter) {
-        if(sentence.contains("capteur") && sentence.contains("numéro")) {
+        Toast errorToast = Toast.makeText(fragment.getActivity().getApplicationContext(),
+                "Votre phrase doit contenir : active/activer/désactive/désactiver et numéro index", Toast.LENGTH_LONG);
 
+        if(isValid(sentence)) {
             Map<String, String> map = getWords(sentence);
+
             String index = map.get("index");
             String value = map.get("activated");
 
-            Boolean activated;
-            switch (value) {
-                case "active":
-                    activated = true;
-                    break;
-                case "désactive":
-                    activated = false;
-                    break;
-                default:
-                    activated = false;
-            }
+            System.out.println("#####"+value);
 
-            Capteur capteur = adapter.getItem(Integer.valueOf(index) - 1);
-            activateCapteur(capteur.getName(), activated);
-        } else {
-            Toast.makeText(fragment.getActivity().getApplicationContext(), "Dire : active/désactive le capteur numéro 1/2...", Toast.LENGTH_LONG).show();
-        }
+            if(index != null) {
+                Boolean activated;
+                switch (value) {
+                    case "activer":
+                    case "active":
+                        activated = true;
+                        break;
+                    case "désactiver":
+                    case "désactive":
+                        activated = false;
+                        break;
+                    default:
+                        activated = false;
+                }
+
+                Capteur capteur = adapter.getItem(Integer.valueOf(index) - 1);
+                activateCapteur(capteur.getName(), activated);
+            } else errorToast.show();
+        } else errorToast.show();
+    }
+
+    private boolean isValid(String sentence) {
+        //Espaces faits exprès
+        return ((sentence.contains(" active ") ||
+                sentence.contains(" désactive ") ||
+                sentence.contains(" activer ") ||
+                sentence.contains(" désactiver ")) &&
+                sentence.contains(" numéro "));
     }
 
     private Map<String, String> getWords(String sentence) {
         String[] sentenceSplitted = sentence.split(" ");
         Map<String, String> map = new HashMap<>();
-        map.put("activated", sentenceSplitted[0]);
-        map.put("index", sentenceSplitted[sentenceSplitted.length-1]);
+
+        for(int i=0; i<sentenceSplitted.length; i++) {
+            switch (sentenceSplitted[i]) {
+                case "activer":
+                case "active":
+                    map.put("activated", sentenceSplitted[i]);
+                    break;
+                case "désactiver":
+                case "désactive":
+                    map.put("activated", sentenceSplitted[i]);
+                    break;
+                case "numéro":
+                    if(i < sentenceSplitted.length-1) map.put("index", sentenceSplitted[i+1]);
+                    break;
+                default:
+            }
+        }
 
         return map;
     }
