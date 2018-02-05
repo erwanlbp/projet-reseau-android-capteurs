291593b55127c76e3baa71c4071559dd0b674b7a
Alex1664 <alex.vacheret@free.fr>
2018-02-05 15:42:34 +0100

    test handleData / lightCapteur

--- gencapteurs/src/main/java/model/HandleData.java
+++ gencapteurs/src/main/java/model/HandleData.java
@@ -1,7 +1,19 @@
 package model;
 
+/**
+ * Class handledata.
+ */
 public interface HandleData {
+
+    /**
+     * Genere les données random.
+     * @return
+     */
     double generateData();
 
+    /**
+     * Retourne la valeur de la données du capteur.
+     * @return
+     */
     double getData();
 }
