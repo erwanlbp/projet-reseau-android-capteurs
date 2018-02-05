10eb20536bd8f50f3878115408fa2c4d2cefcad8
Alex1664 <alex.vacheret@free.fr>
2018-02-05 15:42:34 +0100

    test handleData / lightCapteur

--- gencapteurs/src/main/java/model/LightCapteur.java
+++ gencapteurs/src/main/java/model/LightCapteur.java
@@ -2,25 +2,55 @@ package model;
 
 import com.google.firebase.database.Exclude;
 
+/**
+ * Classe de capteur de lumiere.
+ */
 public class LightCapteur extends Capteur {
 
+    /**
+     * Range pour générer data.
+     */
+    private static final int RANGE = 3;
+
+    /**
+     * Valeur du capteur.
+     */
     private double data;
 
+    /**
+     * Constructeur vide.
+     */
     public LightCapteur() {
         super();
     }
 
-    public LightCapteur(String name, Type type, boolean activated, double data) {
+    /**
+     * Constructeur.
+     *
+     * @param name      nom du capteur
+     * @param type      type du capteur
+     * @param activated si il est actif ou non
+     * @param pdata      données d'initialisation
+     */
+    public LightCapteur(final String name, final Type type, final boolean activated, final double pdata) {
         super(name, type, activated);
-        this.data = data;
+        this.data = pdata;
     }
 
+    /**
+     * Genere les data aleatoirement.
+     * @return
+     */
     @Override
     public double generateData() {
-        data = getFromRange(data, 3);
+        data = getFromRange(data, RANGE);
         return data;
     }
 
+    /**
+     * Getter pour la données.
+     * @return
+     */
     @Exclude
     @Override
     public double getData() {
