b16df2578de02e544b7bb255f5a24ac606585517
erwanlbp <erwan.lbp@gmail.com>
2018-02-05 15:42:30 +0100

    DBInterfaceController

--- gencapteurs/src/main/java/controlers/dbinterface/DBInterfaceController.java
+++ gencapteurs/src/main/java/controlers/dbinterface/DBInterfaceController.java
@@ -14,24 +14,54 @@ import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Pour lancer le programme en interface DB.
+ */
 public class DBInterfaceController {
 
+    /**
+     * Le port d'écoute.
+     */
     private int portListen;
+    /**
+     * Pour parser et formatter les objets en JSON.
+     */
     private ObjectMapper objectMapper;
+    /**
+     * La liste des capteurs du réseau.
+     */
     private List<String> listCapteur;
 
-    public DBInterfaceController(int portListen) throws IOException, InterruptedException {
-        this.portListen = portListen;
+    /**
+     * Constructeur à utiliser.
+     *
+     * @param pportListen Le port d'écoute
+     * @throws IOException          Exception
+     * @throws InterruptedException Exception
+     */
+    public DBInterfaceController(final int pportListen) throws IOException, InterruptedException {
+        this.portListen = pportListen;
         objectMapper = new ObjectMapper();
         listCapteur = new ArrayList<>();
     }
 
+    /**
+     * Pour lancer l'écoute des start/stop sur Firebase.
+     *
+     * @throws IOException          Exception
+     * @throws InterruptedException Exception
+     */
     public void init() throws IOException, InterruptedException {
         FirebaseClient.getInstance().listenStartStop((capteur) -> sendStartStop(capteur));
         receiveFlux();
     }
 
-    private void sendStartStop(Capteur capteur) {
+    /**
+     * Pour envoyer un start/stop sur le réseau.
+     *
+     * @param capteur Le capteur à stopper
+     */
+    private void sendStartStop(final Capteur capteur) {
         int serverPort = NetworkConfig.getInstance().getPortDestGenCapteurs();
         String msg = capteur.getName() + " : " + capteur.isActivated();
 
@@ -48,6 +78,9 @@ public class DBInterfaceController {
         }
     }
 
+    /**
+     * Ecoute les flux du réseau de capteurs.
+     */
     private void receiveFlux() {
         DatagramSocket client;
 
@@ -67,7 +100,14 @@ public class DBInterfaceController {
         }
     }
 
-    private void analyseAndSendPacket(DatagramPacket packet) throws IOException, InterruptedException {
+    /**
+     * Analyse le paquet recu du réseau.
+     *
+     * @param packet Le paquet UDP
+     * @throws IOException          Exception
+     * @throws InterruptedException Exception
+     */
+    private void analyseAndSendPacket(final DatagramPacket packet) throws IOException, InterruptedException {
         Capteur capteur = null;
         boolean found = false;
 
