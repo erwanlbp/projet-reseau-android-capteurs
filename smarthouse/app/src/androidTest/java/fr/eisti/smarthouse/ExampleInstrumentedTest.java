effd3bbd6fb7963f28519c162dbdd163c50b9cb2
Alex1664 <alex.vacheret@free.fr>
2018-02-06 10:29:40 +0100

    instrumented test smarthouse

--- smarthouse/app/src/androidTest/java/fr/eisti/smarthouse/ExampleInstrumentedTest.java
+++ smarthouse/app/src/androidTest/java/fr/eisti/smarthouse/ExampleInstrumentedTest.java
@@ -7,6 +7,9 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +19,18 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("fr.eisti.smarthouse", appContext.getPackageName());
+    }
+
+    @Test
+    public void testButtonEnabled() throws Exception {
+        onView(withId(R.id.fec_activ_capteur))
+                .perform(click())
+                .check
     }
 }
